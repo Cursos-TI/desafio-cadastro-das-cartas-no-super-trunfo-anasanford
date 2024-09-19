91ed5544fce5c9c0ec901e5fd5de30e002ba1f7c
anasanford <anabeatrizsanford@gmail.com>
2024-09-19 23:33:19 +0000

    Adicionei densidade populacional e PIB per capta

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,6 +5,7 @@ int main() {
     //criação das variáveis
     int populacao, turistico;
     float area, pib;
+    double densidadepopul, pibpercapta;
     char cidade[20];
     char estado;
     char codigo[5];
@@ -23,11 +24,11 @@ int main() {
     printf("Digite o código da cidade (letra + 01-04): \n");
     scanf("%s", codigo);
 
-    printf("Digite a população da cidade: \n");
+    printf("Digite a população da cidade (valor total): \n");
     scanf("%d", &populacao);
 
     printf("Digite a área da cidade (em km2): \n");
-    scanf("%f", &area);
+    scanf(" %f", &area);
 
     printf("Digite o PIB da cidade (em milhões de reais): \n");
     scanf("%f", &pib);
@@ -35,16 +36,21 @@ int main() {
     printf("Digite o número de pontos turísticos da cidade: \n");
     scanf("%d", &turistico);
 
+    //calculando a densidade populacional e o PIB per capta
+    densidadepopul = populacao / area;
+    pibpercapta = pib / populacao;
+
     //mostrando o resultado na tela para o usuário
-    printf("-------------------------- \n");
-    printf("Cidade: %s \n", cidade);
+    printf("-------- %s --------- \n", cidade);
     printf("Estado: %c \n", estado);
     printf("Código: %s \n", codigo);
     printf("População: %d habitantes \n", populacao);
     printf("Área: %.2f km2 \n", area);
     printf("PIB: %.3f milhões de reais \n", pib);
     printf("Número de pontos turísticos: %d \n", turistico);
-    printf("-------------------------- \n");
+    printf("Densidade populacional: %.2lf habitantes/km2 \n", densidadepopul);
+    printf("PIB per capta: %.3lf milhões/habitante \n", pibpercapta);
+    printf("------------------------------------ \n");
 
     return 0;
 }
